9e5349aa7eafc119c13ca59430df167e0b5e2c5f
lch <574582775@qq.com>
2018-09-13 00:20:35 +0800

    demo2
    demo2完成

--- ProjectOpengl/Source.cpp
+++ ProjectOpengl/Source.cpp
@@ -3,8 +3,20 @@
 #include <GLFW/glfw3.h>
 
 
+/*********************************DEMO 3
+在OpenGL中，任何事物都在3D空间中，而屏幕和窗口却是2D像素数组，这导致OpenGL的大部分工作都是关于把3D坐标转变为适应你屏幕的2D像素。3D坐标转为2D坐标的处理过程是由OpenGL的图形渲染管线（Graphics Pipeline，大多译为管线，实际上指的是一堆原始图形数据途经一个输送管道，期间经过各种变化处理最终出现在屏幕的过程）管理的。图形渲染管线可以被划分为两个主要部分：第一部分把你的3D坐标转换为2D坐标，第二部分是把2D坐标转变为实际的有颜色的像素。这个教程里，我们会简单地讨论一下图形渲染管线，以及如何利用它创建一些漂亮的像素。*/
+
+//https://learnopengl-cn.github.io/01%20Getting%20started/04%20Hello%20Triangle/
+
+
+
+
+
+
+/************************* DEMO 2
 //当用户改变窗口的大小的时候，视口也应该被调整。回调
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
 
 
 int main()
@@ -15,8 +27,8 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     //告诉GLFW我们使用的是核心模式(Core-profile)。
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    /*如果使用的是Mac OS X系统，你还需要加下面这行代码到你的初始化代码中这些配置才能起作用（将下面的代码解除注释）：
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);*/
+    //如果使用的是Mac OS X系统，你还需要加下面这行代码到你的初始化代码中这些配置才能起作用（将下面的代码解除注释）：
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
 
     GLFWwindow* window = glfwCreateWindow(400, 300, "First OpenGL window", NULL, NULL);
@@ -36,17 +48,36 @@ int main()
         return -1;
     }
 
-    /*在我们开始渲染之前还有一件重要的事情要做，我们必须告诉OpenGL渲染窗口的尺寸大小，即视口(Viewport)，这样OpenGL才只能知道怎样根据窗口大小显示数据和坐标。我们可以通过调用glViewport函数来设置窗口的维度(Dimension)：*/
-    /*OpenGL幕后使用glViewport中定义的位置和宽高进行2D坐标的转换，将OpenGL中的位置坐标转换为你的屏幕坐标。例如，OpenGL中的坐标(-0.5, 0.5)有可能（最终）被映射为屏幕中的坐标(200, 450)。注意，处理过的OpenGL坐标范围只为 - 1到1，因此我们事实上将(-1到1)范围内的坐标映射到(0, 800)和(0, 600)。*/
+    //在我们开始渲染之前还有一件重要的事情要做，我们必须告诉OpenGL渲染窗口的尺寸大小，即视口(Viewport)，这样OpenGL才只能知道怎样根据窗口大小显示数据和坐标。我们可以通过调用glViewport函数来设置窗口的维度(Dimension)：
+    //OpenGL幕后使用glViewport中定义的位置和宽高进行2D坐标的转换，将OpenGL中的位置坐标转换为你的屏幕坐标。例如，OpenGL中的坐标(-0.5, 0.5)有可能（最终）被映射为屏幕中的坐标(200, 450)。注意，处理过的OpenGL坐标范围只为 - 1到1，因此我们事实上将(-1到1)范围内的坐标映射到(0, 800)和(0, 600)。
     glViewport(0, 0, 400, 300);
 
+    //当用户改变窗口的大小的时候，视口也应该被调整。回调
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    //检测窗口是否关闭
     while (!glfwWindowShouldClose(window))
     {
+        // 输入
+        processInput(window);
+
+        // 渲染指令
+        //glClearColor函数是一个状态设置函数，\
+        而glClear函数则是一个状态使用的函数， 它使用了当前的状态来获取应该清除为的颜色。\
+        我们使用一个自定义的颜色清空屏幕。在每个新的渲染迭代开始的时候我们总是希望清屏，\
+        否则我们仍能看见上一次迭代的渲染结果
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        //可能的缓冲位有GL_COLOR_BUFFER_BIT，GL_DEPTH_BUFFER_BIT和GL_STENCIL_BUFFER_BIT
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // 检查并调用事件，交换缓冲
+        //会交换颜色缓冲（它是一个储存着GLFW窗口每一个像素颜色值的大缓冲）
         glfwSwapBuffers(window);
+        //函数检查有没有触发什么事件（比如键盘输入、鼠标移动等）、更新窗口状态，并调用对应的回调函数
         glfwPollEvents();
     }
+    //当渲染循环结束后我们需要正确释放/删除之前的分配的所有资源。
+    glfwTerminate();
     return 0;
 }
 
@@ -58,6 +89,15 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 }
 
 
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+
+
+********************************************************************************/
 
 
 
@@ -78,12 +118,7 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 
 
 
-
-
-
-
-
-/************************* DEMO ONE
+/************************* DEMO 1
 // GLEW  
 #define GLEW_STATIC  
 #include "GL/glew.h" 
