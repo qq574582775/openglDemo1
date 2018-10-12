331220043e2896b0ff2e460e67e35468e6fd3aa3
lch <574582775@qq.com>
2018-10-12 17:58:43 +0800

    更新

--- ProjectOpengl/Source.cpp
+++ ProjectOpengl/Source.cpp
@@ -1,90 +1,13 @@
-#include <iostream>  
+﻿#include <iostream>  
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 
 /*********************************DEMO 3
-��OpenGL�У��κ����ﶼ��3D�ռ��У�����Ļ�ʹ���ȴ��2D�������飬�⵼��OpenGL�Ĵ󲿷ֹ������ǹ��ڰ�3D����ת��Ϊ��Ӧ����Ļ��2D���ء�3D����תΪ2D����Ĵ�����������OpenGL��ͼ����Ⱦ���ߣ�Graphics Pipeline�������Ϊ���ߣ�ʵ����ָ����һ��ԭʼͼ������;��һ�����͹ܵ����ڼ侭�����ֱ仯�������ճ�������Ļ�Ĺ��̣������ġ�ͼ����Ⱦ���߿��Ա�����Ϊ������Ҫ���֣���һ���ְ����3D����ת��Ϊ2D���꣬�ڶ������ǰ�2D����ת��Ϊʵ�ʵ�����ɫ�����ء�����̳�����ǻ�򵥵�����һ��ͼ����Ⱦ���ߣ��Լ��������������һЩƯ�������ء�*/
+在OpenGL中，任何事物都在3D空间中，而屏幕和窗口却是2D像素数组，这导致OpenGL的大部分工作都是关于把3D坐标转变为适应你屏幕的2D像素。3D坐标转为2D坐标的处理过程是由OpenGL的图形渲染管线（Graphics Pipeline，大多译为管线，实际上指的是一堆原始图形数据途经一个输送管道，期间经过各种变化处理最终出现在屏幕的过程）管理的。图形渲染管线可以被划分为两个主要部分：第一部分把你的3D坐标转换为2D坐标，第二部分是把2D坐标转变为实际的有颜色的像素。这个教程里，我们会简单地讨论一下图形渲染管线，以及如何利用它创建一些漂亮的像素。*/
+/*为了让OpenGL知道我们的坐标和颜色值构成的到底是什么，OpenGL需要你去指定这些数据所表示的渲染类型。我们是希望把这些数据渲染成一系列的点？一系列的三角形？还是仅仅是一个长长的线？做出的这些提示叫做图元(Primitive)，任何一个绘制指令的调用都将把图元传递给OpenGL。这是其中的几个：GL_POINTS、GL_TRIANGLES、GL_LINE_STRIP。*/
 
 //https://learnopengl-cn.github.io/01%20Getting%20started/04%20Hello%20Triangle/
-
-int main()
-{
-    return 0;
-}
-
-
-
-
-/************************* DEMO 2
-//���û��ı䴰�ڵĴ�С��ʱ���ӿ�ҲӦ�ñ��������ص�
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow *window);
-
-
-int main()
-{
-    glfwInit();
-    //���ǽ����汾��(Major)��Ϊ3�ʹΰ汾��(Minor)����Ϊ3
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    //����GLFW����ʹ�õ��Ǻ���ģʽ(Core-profile)��
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    //���ʹ�õ���Mac OS Xϵͳ���㻹��Ҫ���������д��뵽��ĳ�ʼ����������Щ���ò��������ã�������Ĵ�����ע�ͣ���
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-
-
-    GLFWwindow* window = glfwCreateWindow(400, 300, "First OpenGL window", NULL, NULL);
-
-    if (window == NULL)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-
-    //���Ǹ�GLAD��������������ϵͳ��ص�OpenGL����ָ���ַ�ĺ�����GLFW�����ǵ���glfwGetProcAddress�����������Ǳ����ϵͳ��������ȷ�ĺ�����
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
-
-    //�����ǿ�ʼ��Ⱦ֮ǰ����һ����Ҫ������Ҫ�������Ǳ������OpenGL��Ⱦ���ڵĳߴ��С�����ӿ�(Viewport)������OpenGL��ֻ��֪���������ݴ��ڴ�С��ʾ���ݺ����ꡣ���ǿ���ͨ������glViewport���������ô��ڵ�ά��(Dimension)��
-    //OpenGLĻ��ʹ��glViewport�ж����λ�úͿ��߽���2D�����ת������OpenGL�е�λ������ת��Ϊ�����Ļ���ꡣ���磬OpenGL�е�����(-0.5, 0.5)�п��ܣ����գ���ӳ��Ϊ��Ļ�е�����(200, 450)��ע�⣬��������OpenGL���귶ΧֻΪ - 1��1�����������ʵ�Ͻ�(-1��1)��Χ�ڵ�����ӳ�䵽(0, 800)��(0, 600)��
-    glViewport(0, 0, 400, 300);
-
-    //���û��ı䴰�ڵĴ�С��ʱ���ӿ�ҲӦ�ñ��������ص�
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    //��ⴰ���Ƿ�ر�
-    while (!glfwWindowShouldClose(window))
-    {
-        // ����
-        processInput(window);
-
-        // ��Ⱦָ��
-        //glClearColor������һ��״̬���ú�����\
-        ��glClear��������һ��״̬ʹ�õĺ����� ��ʹ���˵�ǰ��״̬����ȡӦ�����Ϊ����ɫ��\
-        ����ʹ��һ���Զ������ɫ�����Ļ����ÿ���µ���Ⱦ������ʼ��ʱ����������ϣ��������\
-        �����������ܿ�����һ�ε�������Ⱦ���
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        //���ܵĻ���λ��GL_COLOR_BUFFER_BIT��GL_DEPTH_BUFFER_BIT��GL_STENCIL_BUFFER_BIT
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        // ��鲢�����¼�����������
-        //�ύ����ɫ���壨����һ��������GLFW����ÿһ��������ɫֵ�Ĵ󻺳壩
-        glfwSwapBuffers(window);
-        //���������û�д���ʲô�¼�������������롢����ƶ��ȣ������´���״̬�������ö�Ӧ�Ļص�����
-        glfwPollEvents();
-    }
-    //����Ⱦѭ��������������Ҫ��ȷ�ͷ�/ɾ��֮ǰ�ķ����������Դ��
-    glfwTerminate();
-    return 0;
-}
-
-
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
@@ -97,6 +20,171 @@ void processInput(GLFWwindow *window)
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
+
+
+const int WINDOW_WIDTH = 400;
+const int WINDOW_HEIGHT = 300;
+
+
+
+
+// 顶点着色器
+const char* vertexShaderSource =    "#version 330 core\n                            \
+                                    out vec4 FragColor;\n                           \
+                                    void main(){\n                                  \
+                                    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n     \
+                                    }\n\0";
+
+int main()
+{
+    glfwInit();
+    //我们将主版本号(Major)设为3和次版本号(Minor)都设为3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    //告诉GLFW我们使用的是核心模式(Core-profile)。
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
+#endif
+
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "First OpenGL window", NULL, NULL);
+
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    //我们给GLAD传入了用来加载系统相关的OpenGL函数指针地址的函数。GLFW给我们的是glfwGetProcAddress，它根据我们编译的系统定义了正确的函数。
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    //在我们开始渲染之前还有一件重要的事情要做，我们必须告诉OpenGL渲染窗口的尺寸大小，即视口(Viewport)，这样OpenGL才只能知道怎样根据窗口大小显示数据和坐标。我们可以通过调用glViewport函数来设置窗口的维度(Dimension)：
+    //OpenGL幕后使用glViewport中定义的位置和宽高进行2D坐标的转换，将OpenGL中的位置坐标转换为你的屏幕坐标。例如，OpenGL中的坐标(-0.5, 0.5)有可能（最终）被映射为屏幕中的坐标(200, 450)。注意，处理过的OpenGL坐标范围只为 - 1到1，因此我们事实上将(-1到1)范围内的坐标映射到(0, 800)和(0, 600)。
+    glViewport(0, 0, 400, 300);
+
+    //当用户改变窗口的大小的时候，视口也应该被调整。回调
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+
+    //OpenGL仅当3D坐标在3个轴（x、y和z）上都为-1.0到1.0的范围内时才处理它
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f
+    };
+
+    //顶点着色器
+    unsigned int VBO;
+    //使用glGenBuffers函数和一个缓冲ID生成一个VBO对象：
+    glGenBuffers(1, &VBO);
+    //glBindBuffer函数把新创建的缓冲绑定到GL_ARRAY_BUFFER上，，我们使用的任何（在GL_ARRAY_BUFFER目标上的）缓冲调用都会用来配置当前绑定的缓冲(VBO)
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    //调用glBufferData函数，它会把之前定义的顶点数据复制到缓冲的内存中：
+    /*GL_STATIC_DRAW ：数据不会或几乎不会改变。
+    •GL_DYNAMIC_DRAW：数据会被改变很多。
+    •GL_STREAM_DRAW ：数据每次绘制时都会改变。*/
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    //编译着色器
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);//把着色器源代码编译成目标代码
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);//检查是否编译成功
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+
+
+    return 0;
+}
+
+
+
+
+/************************* DEMO 2
+//当用户改变窗口的大小的时候，视口也应该被调整。回调
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+
+int main()
+{
+    glfwInit();
+    //我们将主版本号(Major)设为3和次版本号(Minor)都设为3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    //告诉GLFW我们使用的是核心模式(Core-profile)。
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //如果使用的是Mac OS X系统，你还需要加下面这行代码到你的初始化代码中这些配置才能起作用（将下面的代码解除注释）：
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+
+    GLFWwindow* window = glfwCreateWindow(400, 300, "First OpenGL window", NULL, NULL);
+
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    //我们给GLAD传入了用来加载系统相关的OpenGL函数指针地址的函数。GLFW给我们的是glfwGetProcAddress，它根据我们编译的系统定义了正确的函数。
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    //在我们开始渲染之前还有一件重要的事情要做，我们必须告诉OpenGL渲染窗口的尺寸大小，即视口(Viewport)，这样OpenGL才只能知道怎样根据窗口大小显示数据和坐标。我们可以通过调用glViewport函数来设置窗口的维度(Dimension)：
+    //OpenGL幕后使用glViewport中定义的位置和宽高进行2D坐标的转换，将OpenGL中的位置坐标转换为你的屏幕坐标。例如，OpenGL中的坐标(-0.5, 0.5)有可能（最终）被映射为屏幕中的坐标(200, 450)。注意，处理过的OpenGL坐标范围只为 - 1到1，因此我们事实上将(-1到1)范围内的坐标映射到(0, 800)和(0, 600)。
+    glViewport(0, 0, 400, 300);
+
+    //当用户改变窗口的大小的时候，视口也应该被调整。回调
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    //检测窗口是否关闭
+    while (!glfwWindowShouldClose(window))
+    {
+        // 输入
+        processInput(window);
+
+        // 渲染指令
+        //glClearColor函数是一个状态设置函数，\
+        而glClear函数则是一个状态使用的函数， 它使用了当前的状态来获取应该清除为的颜色。\
+        我们使用一个自定义的颜色清空屏幕。在每个新的渲染迭代开始的时候我们总是希望清屏，\
+        否则我们仍能看见上一次迭代的渲染结果
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        //可能的缓冲位有GL_COLOR_BUFFER_BIT，GL_DEPTH_BUFFER_BIT和GL_STENCIL_BUFFER_BIT
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // 检查并调用事件，交换缓冲
+        //会交换颜色缓冲（它是一个储存着GLFW窗口每一个像素颜色值的大缓冲）
+        glfwSwapBuffers(window);
+        //函数检查有没有触发什么事件（比如键盘输入、鼠标移动等）、更新窗口状态，并调用对应的回调函数
+        glfwPollEvents();
+    }
+    //当渲染循环结束后我们需要正确释放/删除之前的分配的所有资源。
+    glfwTerminate();
+    return 0;
+}
+
+
+
 
 
 
